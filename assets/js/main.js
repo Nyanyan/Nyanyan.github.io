f3417729b6f7641b96193c3f122f3a3086f71f1d
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-02-28 17:49:27 +0900

    moodified

--- assets/js/main.js
+++ assets/js/main.js
@@ -404,10 +404,10 @@
 						$main._show(location.hash.substr(1), true);
 					});
 			
-					
+
 			//Language
 			if (language == "jp")
-				window.location.href = "https://nyanyan.github.io/ja";
+				window.location.href = "https://nyanyan.github.io/en";
 			else
 				window.location.href = "https://nyanyan.github.io/ja";
 			
