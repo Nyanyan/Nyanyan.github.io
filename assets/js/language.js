c8cab7a8cb648a309d13a088d8e733fdf26ef539
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-02-28 18:30:13 +0900

    moodified

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,12 +1,12 @@
 (function($) {
     //Language
-		var language = (window.navigator.languages && window.navigator.languages[0]) ||
-		window.navigator.language ||
-		window.navigator.userLanguage ||
-		window.navigator.browserLanguage;
+    var language = (window.navigator.languages && window.navigator.languages[0]) ||
+    window.navigator.language ||
+    window.navigator.userLanguage ||
+    window.navigator.browserLanguage;
 
-		if (language == 'ja')
-            window.location.href = "https://nyanyan.github.io/ja/";
-        else
-            window.location.href = "https://nyanyan.github.io/en/";
+    if (language == 'ja')
+        window.location.href = "https://nyanyan.github.io/ja/";
+    else
+        window.location.href = "https://nyanyan.github.io/en/";
 })(jQuery);
